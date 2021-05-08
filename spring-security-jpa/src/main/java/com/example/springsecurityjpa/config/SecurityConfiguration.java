d6a83a88d0d03524609101ed95f926d0afb3826f
skyadav <skyadav@operative.com>
2021-05-08 21:25:14 +0530

    springboot tutorial

---
+++ spring-security-jpa/src/main/java/com/example/springsecurityjpa/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.springsecurityjpa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN_ROLE")
+                .antMatchers("/user").hasAnyRole("ROLE_USER","ADMIN_ROLE")
+                .antMatchers("/").permitAll().and().formLogin();
+    }
+}
---
+++ spring-security-jpa/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (username, password, enabled)
+  values ('user',
+    '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.AQubh4a',
+    1);
+    INSERT INTO users (username, password, enabled)
+      values ('admin',
+        '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.AQubh4a',
+        1);
+
+INSERT INTO authorities (username, authority)
+  values ('user', 'ROLE_USER');
+  INSERT INTO authorities (username, authority)
+    values ('admin', 'ADMIN_ROLE');
